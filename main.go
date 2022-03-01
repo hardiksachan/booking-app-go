a8e538e467d604dbddfb287124506bbd8c56b916
Hardik Sachan <sachanhardik@gmail.com>
2022-03-01 19:02:42 +0530

    finished for today

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	conferenceName := "Go Conference"
@@ -12,30 +15,47 @@ func main() {
 	fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets now!")
 
-	var firstName string
-	var lastName string
-	var email string
-	var userTickets uint
+	for remainingTickets > 0 {
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets uint
 
-	fmt.Print("Enter your first name: ")
-	fmt.Scan(&firstName)
+		fmt.Print("Enter your first name: ")
+		fmt.Scan(&firstName)
 
-	fmt.Print("Enter your last name: ")
-	fmt.Scan(&lastName)
+		fmt.Print("Enter your last name: ")
+		fmt.Scan(&lastName)
 
-	fmt.Print("Enter your email address: ")
-	fmt.Scan(&email)
+		fmt.Print("Enter your email address: ")
+		fmt.Scan(&email)
 
-	fmt.Print("Enter the number of tickets: ")
-	fmt.Scan(&userTickets)
+		fmt.Print("Enter the number of tickets: ")
+		fmt.Scan(&userTickets)
 
-	remainingTickets = remainingTickets - userTickets
-	bookings = append(bookings, firstName + " " + lastName)
+		isInputValid := true
+		isInputValid = isInputValid && len(firstName) >= 2
+		isInputValid = isInputValid && len(lastName) >= 2
+		isInputValid = isInputValid && strings.Contains(email, "@")
+		isInputValid = isInputValid && userTickets > 0 && userTickets > remainingTickets
 
-	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v.\n",
-		firstName, lastName, userTickets, email,
-	)
-	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+		if !isInputValid {
+			fmt.Println("Invalid Input. Please try again")
+			continue
+		}
 
-	fmt.Printf("These are all our bookings: %v\n", bookings)
+		remainingTickets = remainingTickets - userTickets
+		bookings = append(bookings, firstName+" "+lastName)
+
+		fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v.\n",
+			firstName, lastName, userTickets, email,
+		)
+		fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+
+		firstNames := []string{}
+		for _, booking := range bookings {
+			firstNames = append(firstNames, strings.Fields(booking)[0])
+		}
+		fmt.Printf("These are all our bookings: %v\n", firstNames)
+	}
 }
